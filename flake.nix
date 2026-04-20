de2a8b1d0e35c0ed63aee1598dedecfa9977d2c5
ZloyKot <zloyk0t@proton.me>
2026-04-20 12:44:39 +0300

    batman

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Stuff for NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, nixpkgs-unstable, ... }:
+    let
+      unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+    in
+    {
+      nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./pc-hardware.nix
+          ./configuration.nix
+          ./packages.nix
+
+          (
+            { pkgs, lib, ... }:
+            {
+              networking.hostName = "pr1nc3ofcats";
+            }
+          )
+        ];
+
+        specialArgs = {
+        	inherit unstable;
+        };
+      };
+    };
+}
---
+++ packages.nix
@@ -0,0 +1,47 @@
+{ pkgs, unstable, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    micro
+    git
+
+    # taskbar
+    discord
+    vscode
+    wezterm
+    vivaldi
+    vivaldi-ffmpeg-codecs
+    telegram-desktop
+
+    # tak prosto
+    onlyoffice-desktopeditors
+    clementine
+    keepassxc
+    inkscape
+    qbittorrent
+    lutris
+    obs-studio
+
+    # utils
+    audacity
+    ventoy-full
+    btop
+    lazygit
+    nerd-fonts.jetbrains-mono
+    fastfetch
+    fzf
+    zsh
+    zip
+    unzip
+    stow
+    cloudflare-warp
+    gh
+  ] ++ (with unstable; [
+    # unstable packages
+    hexchat	
+  ]
+  );
+
+  nixpkgs.config.permittedInsecurePackages = [
+  	"ventoy-1.1.10"
+  ];
+}

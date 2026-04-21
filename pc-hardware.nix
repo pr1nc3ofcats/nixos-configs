31bcfd043c320b6d7829c9ad6806208329d3a4bf
ZloyKot <zloyk0t@proton.me>
2026-04-21 19:38:35 +0300

    pipewire fixes

--- pc-hardware.nix
+++ pc-hardware.nix
@@ -36,6 +36,26 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  services.pipewire.extraConfig.pipewire-pulse."99-custom.conf" = {
+    "pulse.cmd" = [
+      {
+        cmd = "load-module";
+        args = "module-null-sink sink_name=virtual_sink sink_properties=device.description=\"Virtual_Stereo\"";
+        flags = [ ];
+      }
+      {
+        cmd = "load-module";
+        args = "module-loopback source=virtual_sink.monitor sink=alsa_output.pci-0000_07_00.6.analog-stereo";
+        flags = [ ];
+      }
+      {
+        cmd = "load-module";
+        args = "module-remap-source master=alsa_input.pci-0000_07_00.6.analog-stereo source_name=MICROPHONE source_properties=device.description=MICROPHONE";
+        flags = [ ];
+      }
+    ];
+  };
+
   systemd.tpm2.enable = false;
   boot.initrd.systemd.tpm2.enable = false;
 }
